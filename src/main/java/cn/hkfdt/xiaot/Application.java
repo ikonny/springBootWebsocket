d28bb7442e3c30b9924dc6abef561edb4794a307
zhaopingfei <zhaopingfei@hkfdt.cn>
2017-01-11 16:05:27 +0800

    部署的修改

--- src/main/java/cn/hkfdt/xiaot/Application.java
+++ src/main/java/cn/hkfdt/xiaot/Application.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -20,58 +22,63 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 @SpringBootApplication
-@MapperScan(basePackages={"com.hxkj.waychat.dao"})
+@MapperScan(basePackages = {"com.hxkj.waychat.dao"})
 @ServletComponentScan
-public class Application extends SpringBootServletInitializer{
+public class Application extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
 
-	private static Logger logger = Logger.getLogger(Application.class);
+    private static Logger logger = Logger.getLogger(Application.class);
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(Application.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 
-	@Bean
-	@ConfigurationProperties(prefix = "spring.datasource")
-	public DataSource dataSource() {
-		//org.apache.tomcat.jdbc.pool.DataSource
-		return new org.apache.tomcat.jdbc.pool.DataSource();
-	}
-	
-	@Bean
-	public SqlSessionFactory sqlSessionFactoryBean(@Autowired DataSource dataSource) throws Exception {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        //org.apache.tomcat.jdbc.pool.DataSource
+        return new org.apache.tomcat.jdbc.pool.DataSource();
+    }
 
-		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-		sqlSessionFactoryBean.setDataSource(dataSource);
+    @Bean
+    public SqlSessionFactory sqlSessionFactoryBean(@Autowired DataSource dataSource) throws Exception {
 
-		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
 
 //		sqlSessionFactoryBean.setConfigLocation(resolver.getResource("classpath:/mybatis/config.xml"));
 //		sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mapper/*Mapper.xml"));
-		// classpath*:**/*Mapper.xml
-		Resource[] mapperLocations = resolver.getResources("classpath*:**/*Mapper.xml");//{"classpath*:**/*Mapper.xml"};
-		sqlSessionFactoryBean.setMapperLocations(mapperLocations);
-		//这个就是生成model的地址包
-		sqlSessionFactoryBean.setTypeAliasesPackage("cn.hkfdt.xiaot.mybatis.model");
-		SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBean.getObject();
-		//--------------------
-		return sqlSessionFactory;
-	}
-	
-	@SuppressWarnings("deprecation")
-	@Bean
-	public MapperScannerConfigurer mapperScannerConfigurer(@Autowired SqlSessionFactory sqlSessionFactory) {
-		MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
-		mapperScannerConfigurer.setSqlSessionFactory(sqlSessionFactory);
-		mapperScannerConfigurer.setBasePackage("cn.hkfdt.xiaot.mybatis.mapper");
-		return mapperScannerConfigurer;
-	}
-	
-	//===============以上就是原本定义在xml里面的bean=================================
+        // classpath*:**/*Mapper.xml
+        Resource[] mapperLocations = resolver.getResources("classpath*:**/*Mapper.xml");//{"classpath*:**/*Mapper.xml"};
+        sqlSessionFactoryBean.setMapperLocations(mapperLocations);
+        //这个就是生成model的地址包
+        sqlSessionFactoryBean.setTypeAliasesPackage("cn.hkfdt.xiaot.mybatis.model");
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBean.getObject();
+        //--------------------
+        return sqlSessionFactory;
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-		logger.info("============= SpringBoot Start Success =============");
-	}
+    @SuppressWarnings("deprecation")
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer(@Autowired SqlSessionFactory sqlSessionFactory) {
+        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+        mapperScannerConfigurer.setSqlSessionFactory(sqlSessionFactory);
+        mapperScannerConfigurer.setBasePackage("cn.hkfdt.xiaot.mybatis.mapper");
+        return mapperScannerConfigurer;
+    }
+
+    //===============以上就是原本定义在xml里面的bean=================================
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        logger.info("============= SpringBoot Start Success =============");
+    }
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(12306);
+    }
 
 }
