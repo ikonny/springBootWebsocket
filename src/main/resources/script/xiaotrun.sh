2ad46e2e417c2be52563d3fef1de5de136b6c694
whyse <xuwhyse@163.com>
2017-03-10 15:47:02 +0800

    测试和开发的运行脚本变更

--- src/main/resources/script/xiaotrun.sh
+++ src/main/resources/script/xiaotrun.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 source /etc/profile
 JRE_HOME=/usr/java/jdk1.8.0_121
-JAR_HOME=/root/xiaot/xiaot-boot-0.0.1-SNAPSHOT.jar
+JAR_HOME=~/xiaot/xiaot-boot-0.0.1-SNAPSHOT.jar
 
 P_ID=`jps|grep xiaot|grep -v grep|awk '{print $1}'`
 if [ "$P_ID" == "" ]; then
@@ -11,6 +11,6 @@ echo "=== begin kill XiaoT process, pid is:$P_ID ==="
 kill  -9 $P_ID
 fi
 
-$JRE_HOME/bin/java -d64 -server -Xms2G -Xmx2G -XX:+UseG1GC -jar $JAR_HOME  > /root/xiaot/xiaot.log 2>&1 &
+$JRE_HOME/bin/java -d64 -server -Xms2G -Xmx2G -XX:+UseG1GC -jar $JAR_HOME  > ~/xiaot/xiaot.log 2>&1 &
 #tail -f xiaot.log
 exit 0
